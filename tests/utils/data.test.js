15f5d4c62293b352e5dba3040733831832665a57
NaufalK25 <naufalkateni2001@gmail.com>
2023-02-09 18:38:58 +0700

    instead of data.test.js, remove PDF.test.js

--- tests/utils/data.test.js
+++ tests/utils/data.test.js
@@ -1,64 +1,60 @@
-// const fs = require('fs');
-// const path = require('path');
-// const { rootDir } = require('../../utils/constant');
-// const { addData, getData, isDataExist, removeData } = require('../../utils/data');
+const fs = require('fs');
+const path = require('path');
+const { rootDir } = require('../../utils/constant');
+const { addData, getData, isDataExist, removeData } = require('../../utils/data');
 
-// test('json file exists', () => {
-//     const jsonPath = path.join(rootDir, 'tests', 'data.json');
+test('json file exists', () => {
+    const jsonPath = path.join(rootDir, 'tests', 'data.json');
 
-//     expect(isDataExist('test.pdf', jsonPath)).toBe(false);
+    expect(isDataExist('test.pdf', jsonPath)).toBe(false);
 
-//     addData(
-//         'test.pdf',
-//         {
-//             checksum: 'test',
-//             publicKey: 'test'
-//         },
-//         jsonPath
-//     );
+    addData(
+        'test.pdf',
+        {
+            checksum: 'test',
+            publicKey: 'test'
+        },
+        jsonPath
+    );
 
-//     const data = getData('test.pdf', jsonPath);
+    const data = getData('test.pdf', jsonPath);
 
-//     expect(isDataExist('test.pdf', jsonPath)).toBe(true);
-//     expect(data).toEqual({
-//         checksum: 'test',
-//         publicKey: 'test'
-//     });
+    expect(isDataExist('test.pdf', jsonPath)).toBe(true);
+    expect(data).toEqual({
+        checksum: 'test',
+        publicKey: 'test'
+    });
 
-//     removeData('test.pdf', jsonPath);
+    removeData('test.pdf', jsonPath);
 
-//     expect(isDataExist('test.pdf', jsonPath)).toBe(false);
-// });
+    expect(isDataExist('test.pdf', jsonPath)).toBe(false);
+});
 
-// test('json file does not exist', () => {
-//     const jsonPath = path.join(rootDir, 'tests', 'data2.json');
+test('json file does not exist', () => {
+    const jsonPath = path.join(rootDir, 'tests', 'data2.json');
 
-//     expect(isDataExist('test.pdf', jsonPath)).toBe(false);
+    expect(isDataExist('test.pdf', jsonPath)).toBe(false);
 
-//     addData(
-//         'test.pdf',
-//         {
-//             checksum: 'test',
-//             publicKey: 'test'
-//         },
-//         jsonPath
-//     );
+    addData(
+        'test.pdf',
+        {
+            checksum: 'test',
+            publicKey: 'test'
+        },
+        jsonPath
+    );
 
-//     const data = getData('test.pdf', jsonPath);
+    const data = getData('test.pdf', jsonPath);
 
-//     expect(isDataExist('test.pdf', jsonPath)).toBe(true);
-//     expect(data).toEqual({
-//         checksum: 'test',
-//         publicKey: 'test'
-//     });
+    expect(isDataExist('test.pdf', jsonPath)).toBe(true);
+    expect(data).toEqual({
+        checksum: 'test',
+        publicKey: 'test'
+    });
 
-//     removeData('test.pdf', jsonPath);
+    removeData('test.pdf', jsonPath);
 
-//     expect(isDataExist('test.pdf', jsonPath)).toBe(false);
+    expect(isDataExist('test.pdf', jsonPath)).toBe(false);
 
-//     fs.unlinkSync(jsonPath);
-// });
-
-test('true is true', () => {
-    expect(true).toBe(true);
+    fs.unlinkSync(jsonPath);
 });
