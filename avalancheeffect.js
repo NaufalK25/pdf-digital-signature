5e92296132ffdcda314d1fdd55b90f7f62cc7843
NaufalK25 <naufalkateni2001@gmail.com>
2023-06-04 14:15:06 +0700

    add avalanche effect

---
+++ avalancheeffect.js
@@ -0,0 +1,48 @@
+const fs = require('fs');
+const path = require('path');
+const AES = require('./utils/AES');
+const BLAKE2s = require('./utils/BLAKE2s');
+const { textToDec } = require('./utils/converter');
+require('dotenv').config();
+
+const getSignature = filename => {
+    const fileBuffer = fs.readFileSync(path.join(__dirname, 'data', filename));
+    const publicKey = 'test'.padEnd(32, ' ');
+    const keyBuffer = new Uint8Array([...publicKey].map(textToDec));
+    return new BLAKE2s(publicKey.length, keyBuffer).update(fileBuffer).hexDigest();
+};
+
+const getEncryptedSignature = signature => {
+    const privateKey = process.env.AES_KEY;
+    const encryptedSignature = new AES(privateKey).encrypt(signature);
+    return textToHex(encryptedSignature);
+};
+
+const textToHex = text => {
+    return [...text].map(char => char.charCodeAt(0).toString(16).padStart(2, '0')).join('');
+};
+
+const countAvalancheEffect = (value1, value2) => {
+    if (value1.length !== value2.length) throw new Error('Panjang kedua nilai harus sama');
+
+    let count = 0;
+    for (let i = 0; i < value1.length; i++) {
+        if (value1[i] !== value2[i]) {
+            count++;
+        }
+    }
+
+    return (count / value1.length) * 100;
+};
+
+const value1 = getSignature('test(8).pdf');
+const value2 = getSignature('test(9).pdf');
+console.log(value1);
+console.log(value2);
+console.log(countAvalancheEffect(value1, value2));
+
+const value3 = getEncryptedSignature(value1);
+const value4 = getEncryptedSignature(value2);
+console.log(value3);
+console.log(value4);
+console.log(countAvalancheEffect(value3, value4));
